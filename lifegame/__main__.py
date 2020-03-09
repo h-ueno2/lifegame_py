d7532a08e3d60ca756689900f1bf4bc9263b6123
h-ueno2 <menbou1212@gmail.com>
2020-03-09 22:36:13 +0900

    mainファイル修正

--- lifegame/__main__.py
+++ lifegame/__main__.py
@@ -1,14 +1,8 @@
 # encoding:utf-8
 from const import Const
 from cell_data import CellData
-from random import randint
 from window import Window
 
-
-def main():
+if __name__ == "__main__":
     cell_data = CellData(Const.ROWS, Const.COLS)
     window = Window(cell_data)
-
-
-if __name__ == "__main__":
-    main()
